63a2638414480eefc5386448344f40d9f5994e77
NathaliaGG <ngrafgr@okstate.edu>
2018-02-14 16:42:33 -0600

    2nd commit, removed files cached

---
+++ ngg_unix_morning/ending_of_unix1.sh
@@ -0,0 +1,4 @@
+# LOL! Did you know you can set up the computer to say things to you?!
+# Just type 'say ...' in the command line, and add a word or phrase.
+
+say "You did a great job today! Let's have lunch!!"
